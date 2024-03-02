2ded0273f26260e35a72128fa68cb116476ab45c
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2024-03-02 19:56:43 -0300

    CHANGE Removed redundant return None statements in the except blocks and other changes

--- netman/connect.py
+++ netman/connect.py
@@ -1,3 +1,4 @@
+from typing import Union
 import telnetlib
 import paramiko
 import platform
@@ -10,43 +11,41 @@ def ping(host: str) -> bool:
         cmd = f"ping {flag} {host}"
         need_sh = platform.system().lower() != "windows"
         return subprocess.run(cmd, shell=need_sh, capture_output=True, check=True).returncode == 0
-    except Exception as e:
+    except subprocess.CalledProcessError:
         return False
 
-def telnet(host: str, username: str, password: str) -> telnetlib.Telnet:
-    print(f"Trying Telnet connection to {host}")
+def telnet(host: str, username: str, password: str) -> Union[telnetlib.Telnet, None]:
     try:
         client = telnetlib.Telnet(host, timeout=2)
         if password:
-            client.write((username + "\n").encode('ascii'))
-            client.write((password + "\n").encode('ascii'))
+            client.write(f"{username}\n{password}\n".encode('ascii'))
         return client
-    except Exception as e:
+    except Exception:
         return None
 
-def ssh(host: str, username: str, password: str) -> paramiko.SSHClient:
-    print(f"Trying SSH connection to {host}")
+def ssh(host: str, username: str, password: str) -> Union[paramiko.SSHClient, None]:
     try:
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
         try:
             client.load_system_host_keys()
             client.connect(hostname=host, username=username, password=password, timeout=5, allow_agent=False)
-            return client if client.get_transport().is_authenticated() else None
         except paramiko.AuthenticationException:
             try:
                 client.connect(hostname=host, username=username, password=password, timeout=5)
-                return client if client.get_transport().is_authenticated() else None
-            except Exception as e:
+            except Exception:
                 return None
-    except Exception as e:
+
+        return client if client.get_transport().is_authenticated() else None
+    except Exception:
         return None
 
-def connect(args) -> None:
+def connect(args) -> Union[telnetlib.Telnet, paramiko.SSHClient, None]:
     host, username, password = args.host, args.username, args.password
 
     if not ping(host):
-        return
+        return None
 
     connection_type = args.connection.lower() if args.connection else None
     connect_functions = [telnet, ssh]
@@ -57,7 +56,10 @@ def connect(args) -> None:
     for connect_func in connect_functions:
         connection = connect_func(host, username, password)
         if connection:
-            print(f"{connect_func.__name__.capitalize()} connection successful")
+            print(f"\n{connect_func.__name__.capitalize()} connection successful")
             return connection
+        else:
+            print("....", end="", flush=True)
 
-    log.error(f"Failed to connect to {host}")
+    print(f"Failed to connect to {host}")
+    return None
