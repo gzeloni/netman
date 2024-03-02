12a255be3f015bc7a0d362128eb94b67edd71fc7
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2024-03-02 19:21:35 -0300

    CHANGE methods names

--- netman/exec_command.py
+++ netman/exec_command.py
@@ -5,19 +5,19 @@ class CommandExec:
     def __init__(self, connection):
         self.connection = connection
         self.command_methods = {
-            Telnet: self._execute_telnet_command,
-            SSHClient: self._execute_ssh_command
+            Telnet: self._exec_telnet_command,
+            SSHClient: self._exec_ssh_command
         }
 
         if type(connection) not in self.command_methods:
             raise ValueError("Unsupported connection type")
 
-    def _execute_telnet_command(self, command):
+    def _exec_telnet_command(self, command):
         self.connection.write(command.encode('ascii') + b"\n")
         output = self.connection.read_until(b">")
         return output.decode('ascii')
 
-    def _execute_ssh_command(self, command):
+    def _exec_ssh_command(self, command):
         stdin, stdout, stderr = self.connection.exec_command(command)
         output = stdout.read()
         return output.decode('utf-8')
