1b482d4779ca7be03ef70aff698ad4e4f0a03d8e
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2024-03-02 19:35:26 -0300

    ADD optional command to select connection type (ssh or telnet)

--- netman/config.py
+++ netman/config.py
@@ -1,5 +1,6 @@
 CONFIGURATIONS = [
     {"name": "-sh", "dest": "host", "type": str, "required": True, "help": "Switch IP address or hostname"},
     {"name": "-u", "dest": "username", "type": str, "required": True, "help": "Username for authentication"},
-    {"name": "-p", "dest": "password", "type": str, "nargs": "?", "const": None, "help": "Password for authentication"}
+    {"name": "-p", "dest": "password", "type": str, "nargs": "?", "const": None, "help": "Password for authentication"},
+    {"name": "-c", "dest": "connection", "choices": ['telnet', 'ssh'], "help": "Specify the type of connection (telnet or ssh)"}
 ]
