70d988e5b39ab6968fce83fd962646a236bc5964
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2024-03-02 19:22:35 -0300

    CHANGE add executor to main to interact with the host

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from netman.parse_args import parser
 from netman.connect import connect
+from netman.exec_command import CommandExec
+
+
 def main():
     args = parser()
-    connect(args=args)
-
+    con = connect(args=args)
+    executor = CommandExec(con)
+    print("Type 'exit' to leave\n")
+    while True:
+        user_input = input("> ")
+        if user_input.lower() == 'exit':
+            break
+        
+        output = executor.exec_command(user_input)
+        print(output)
 
 if __name__ == "__main__":
     main()
