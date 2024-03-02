9f6c5fcf37c49042c166892fc7611970c646e160
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2024-03-02 19:34:55 -0300

    CHANGE handling KeyboardInterrupt

--- main.py
+++ main.py
@@ -2,19 +2,21 @@ from netman.parse_args import parser
 from netman.connect import connect
 from netman.exec_command import CommandExec
 
-
 def main():
-    args = parser()
-    con = connect(args=args)
-    executor = CommandExec(con)
-    print("Type 'exit' to leave\n")
-    while True:
-        user_input = input("> ")
-        if user_input.lower() == 'exit':
-            break
-        
-        output = executor.exec_command(user_input)
-        print(output)
+    try:
+        args = parser()
+        con = connect(args=args)
+        executor = CommandExec(con)
+        print("Type 'exit' to leave\n")
+        while True:
+            user_input = input("> ")
+            if user_input.lower() == 'exit':
+                break
+            
+            output = executor.exec_command(user_input)
+            print(output)
+    except KeyboardInterrupt:
+        print("\nCtrl+C detected. Exiting gracefully.")
 
 if __name__ == "__main__":
     main()
