74f07950cc95092ec51068e40dc3a6153b389162
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2024-03-14 22:01:50 -0300

    CHANGE add connection_types to config file as a constant

--- netman/config.py
+++ netman/config.py
@@ -1,6 +1,11 @@
+CONNECTION_TYPES = ['ssh', 'telnet', 'snmp']
+
 CONFIGURATIONS = [
     {"name": "-sh", "dest": "host", "type": str, "required": True, "help": "Switch IP address or hostname"},
     {"name": "-u", "dest": "username", "type": str, "required": True, "help": "Username for authentication"},
     {"name": "-p", "dest": "password", "type": str, "nargs": "?", "const": None, "help": "Password for authentication"},
-    {"name": "-c", "dest": "connection", "choices": ['telnet', 'ssh'], "help": "Specify the type of connection (telnet or ssh)"}
+    {"name": "-t", "dest": "connection", "choices": CONNECTION_TYPES, "help": "Specify the type of connection (ssh, telnet or snmp)"},
+    {"name": "--option", "dest": "gw", "type": str, "required": False, "help": "SNMP Get or Walk"},
+    {"name": "--community", "dest": "community", "type": str, "required": False, "help": "SNMP Community"},
+    {"name": "--oid", "dest": "oid", "type": str, "required": False, "help": "SNMP MiB or OID"},
 ]
