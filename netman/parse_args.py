8063b92746cc64273fea19ab29377db1528635d4
Gustavo Zeloni <gustavoluizdejesus@gmail.com>
2024-03-14 22:02:50 -0300

    CHANGE parse SNMP flags

--- netman/parse_args.py
+++ netman/parse_args.py
@@ -1,20 +1,25 @@
 import argparse
 import getpass
-from netman.config import CONFIGURATIONS
+from netman.config import *
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="NetMan - Streamlining switch configurations.")
+    parser = argparse.ArgumentParser(description='NetMan - Streamlining switch configurations.')
 
     for config in CONFIGURATIONS:
-        name = config.pop("name")
+        name = config.pop('name')
         parser.add_argument(name, **config)
 
     args = parser.parse_args()
-
-    args.password = getpass.getpass("Enter password: ") if args.password is None else args.password
-
-    valid_connection_types = ['telnet', 'ssh']
-    if args.connection and args.connection.lower() not in valid_connection_types:
-        parser.error(f"Invalid connection type. Use {', '.join(valid_connection_types)}.")
+    
+    if args.connection.lower() not in CONNECTION_TYPES:
+        parser.error(f'Invalid connection type. Use {", ".join(CONNECTION_TYPES)}.')
+        
+    if args.connection.lower() == 'snmp':
+        args.gw = input("Get or Walk?: ").lower()
+        args.community = input("Enter SNMP Community: ")
+        args.oid = input("Enter SNMP OID or MIB: ")
+    
+    args.password = getpass.getpass('Enter password: ') if args.password is None else args.password
+    
 
     return args
